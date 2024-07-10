398d21e721d187de9a057e771f4487bb2f588c55
Venus Waikhom <apocalyse7@gmail.com>
2024-07-10 09:48:22 +0530

    Experiences changes

--- src/components/Exp/ExpItems.tsx
+++ src/components/Exp/ExpItems.tsx
@@ -5,7 +5,7 @@ export const ExpItems = [
     link: "https://github.com/VenusWaikhom/MRCC-Client",
   },
   {
-    name: "E-Commerce(Practice)",
+    name: "E-Commerce",
     text: "While Practicing To develop i have built an E-Commerce Website With full responsive",
     link: "https://github.com/VenusWaikhom/Ecom-FrontEnd",
   },
@@ -13,15 +13,5 @@ export const ExpItems = [
     name: "E-Commerce API",
     text: "Practice Of E-commerse API",
     link: "https://github.com/VenusWaikhom/Traning_Ecom-API",
-  },
-  {
-    name: "Simple IG UI",
-    text: "Practicing to built an IG template",
-    link: "https://github.com/VenusWaikhom/Simple-IG",
-  },
-  {
-    name: "DraftJS",
-    text: "Implementing DraftJS in React",
-    link: "https://github.com/VenusWaikhom/DraftJS",
   },
 ];
