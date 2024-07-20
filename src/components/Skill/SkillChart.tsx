6e1016156a7c54370731ad4065b59f81aedc6a43
Venus Waikhom <apocalyse7@gmail.com>
2024-07-20 11:55:26 +0530

    Skills added and clean up some

--- src/components/Skill/SkillChart.tsx
+++ src/components/Skill/SkillChart.tsx
@@ -5,11 +5,13 @@ export interface SkillChartItem {
 }
 
 export const SkillChart: SkillChartItem[] = [
-  { name: "HTML", icon: "/Images/html-5.png", Level: "98%" },
-  { name: "CSS", icon: "/Images/css-3.png", Level: "95%" },
   { name: "Javascript", icon: "/Images/js.png", Level: "83%" },
+  { name: "Typescript", icon: "/Images/Ts.png", Level: "82%" },
   { name: "React", icon: "/Images/react.png", Level: "80%" },
   { name: "NodeJS", icon: "/Images/nodejs.png", Level: "75%" },
   { name: "MongoDB", icon: "/Images/mongodb.png", Level: "70%" },
+  { name: "MYSQL", icon: "/Images/Mysql.png", Level: "65%" },
+  { name: "C", icon: "/Images/C.png", Level: "60%" },
+  { name: "Cp", icon: "/Images/C++.png", Level: "60%" },
   { name: "Python", icon: "/Images/python.png", Level: "60%" },
 ];
